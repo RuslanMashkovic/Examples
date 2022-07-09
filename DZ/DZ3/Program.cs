c9d0bf43b6469b1593f22b738dc6e668e4fcc21a
Ruslan Mashkovich <abxaz1k23@gmail.com>
2022-07-09 23:27:38 +0300

    add DZ3 Задача 6)

---
+++ DZ/DZ3/Program.cs
@@ -0,0 +1,12 @@
+﻿int a;
+Console.WriteLine("введите число: ");
+a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 2 == 0)
+{
+    Console.WriteLine("четное число");           
+}
+else
+{
+    Console.WriteLine("нечетное");
+}
