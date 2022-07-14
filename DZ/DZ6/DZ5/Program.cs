7c788a2b7ddc4c2c4a84a25827e51ed5d5a6904a
Ruslan Mashkovich <abxaz1k23@gmail.com>
2022-07-14 20:58:13 +0300

    add Задача 13

--- DZ/DZ6/DZ5/Program.cs
+++
@@ -1,4 +0,0 @@
-﻿Console.Write("Введи трёхзначное число: ");
-int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
-string stringNumber = Convert.ToString(threeDigitNumber);
-Console.WriteLine("вторая цифра этого числа -> "+stringNumber[1]);
