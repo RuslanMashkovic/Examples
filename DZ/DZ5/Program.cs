a3a06a63ee52480a14d8b016892864736961759e
Ruslan Mashkovich <abxaz1k23@gmail.com>
2022-07-14 20:38:01 +0300

    add Задача 10

--- DZ/DZ5/Program.cs
+++ DZ/DZ5/Program.cs
@@ -2,3 +2,5 @@
 int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
 string stringNumber = Convert.ToString(threeDigitNumber);
 Console.WriteLine("вторая цифра этого числа -> "+stringNumber[1]);
+
+
